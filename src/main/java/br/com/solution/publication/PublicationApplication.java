37c0cd224164798e938d5c5118058809fdd7391e
Wallace Gonçalves <wallacecosme@gmail.com>
2024-06-10 21:29:28 -0300

    first commit

---
+++ src/main/java/br/com/solution/publication/PublicationApplication.java
@@ -0,0 +1,13 @@
+package br.com.solution.publication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solution/publication/PublicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solution.publication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
